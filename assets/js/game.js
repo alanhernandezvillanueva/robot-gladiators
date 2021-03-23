08042cd0c0b85af3b10d79c5efdb6bc10e1da33c
alanhernandezvillanueva <79774478+alanhernandezvillanueva@users.noreply.github.com>
2021-03-22 23:08:57 -0700

    commit 3.1.5

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,9 @@
-window.alert("thisd is an alert! JavaScript running!");
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+console.log(20 + 20);
+console.log("Our robot's name is " + playerName)
+function fight(){
+    window.alert("The fight has begun!");
+}
+//fight();
